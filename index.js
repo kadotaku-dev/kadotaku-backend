5bfe2917786b03751ff02999676e496d00360fbc
kadotaku <pillet.vincent@orange.fr>
2026-05-06 20:11:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,4 +55,7 @@ app.get("/api/search", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("OK");
+});
 app.listen(PORT, () => console.log("API running on", PORT));
